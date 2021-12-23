db4157017bcc7a80dafdceb4a111ce0917b133c1
Behlole <behlole.aqil@aksa-sds.com>
2021-12-23 22:14:34 +0500

    nav find and create

--- app/Http/Controllers/CMS/NavController.php
+++ app/Http/Controllers/CMS/NavController.php
@@ -21,22 +21,58 @@ class NavController extends Controller
 
     public function createNav(Request $request)
     {
+        $this->validateNavRequest($request);
+        $this->saveOrUpdateNav($request, 0);
+
+    }
+
+    public function updateNav(Request $request, $id)
+    {
+        $this->validateNavRequest($request, $id);
+
+
+    }
+
+    private function validateNavRequest(Request $request)
+    {
         $this->validate($request, [
             'text' => 'required',
             'url' => 'required',
             'is_collapsable' => 'required',
             'parent_id' => 'required',
         ]);
+    }
+
+    private function saveOrUpdateNav(Request $request, $id = null)
+    {
         try {
             $request = $request->only(['text', 'url', 'is_collapsable', 'parent_id']);
-            $nav = new Nav();
-            $nav->fill($request);
-            $nav->save();
-            return $this->successResponse($nav, 'Data Added Successfully');
+            if ($id) {
+                $nav = Nav::find($id);
+                $nav->fill($request);
+                $nav->update();
+                return $this->successResponse($nav, 'Data Updated Successfully');
+            } else {
+                $nav = new Nav();
+                $nav->fill($request);
+                $nav->save();
+                return $this->successResponse($nav, 'Data Added Successfully');
+            }
+
         } catch (\Exception $exception) {
             return $this->errorResponse($exception->getMessage(), null);
 
         }
+    }
 
+    public function deleteNav($id)
+    {
+        $nav = Nav::find($id)->delete();
+        if ($nav) {
+            return $this->successResponse($nav, 'Data Deleted Successfully');
+        } else {
+            return $this->errorResponse('Data Not Found');
+
+        }
     }
 }
